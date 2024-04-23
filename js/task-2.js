1ea37e7473e2da5f3f4f0101faa01dffc1ade34e
yamigakuru <qforceplay@gmail.com>
2024-04-23 10:56:24 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,16 @@
 class Storage {
     #items
     constructor(initialItems){
-        this.items = initialItems
+        this.#items = initialItems
     }
     getItems(){
-        return this.items
+        return this.#items
     }
     addItem(newItem){
-        this.items.push(newItem)
+        this.#items.push(newItem)
     }
    removeItem(itemToRemove){
-    this.items = this.items.filter(item => item !== itemToRemove)
+    this.#items = this.#items.filter(item => item !== itemToRemove)
    }
 }
 
